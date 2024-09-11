dfff48eb0e7d29c0063a572c18e9655ff05ab550
jabolos10 <jablonowskioskar8@gmail.com>
2024-09-11 20:54:11 +0200

    Add files via upload

---
+++ Healthbar.h
@@ -0,0 +1,22 @@
+#ifndef HEALTHBAR_H
+#define HEALTHBAR_H
+
+#include <SFML/Graphics.hpp>
+
+
+class HealthBar {
+public:
+    HealthBar(float x, float y, float width, float height);
+
+    void setHealthPoints(float healthPoints);
+    void draw(sf::RenderWindow& window) const;
+
+private:
+    float maxHealth;
+    float currentHealth;
+    sf::RectangleShape background;
+    sf::RectangleShape foreground;
+};
+
+
+#endif // HEALTHBAR_H
